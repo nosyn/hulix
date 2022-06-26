fc122dbab3ebcbf96e8dce4939364a32473a4c99
sonplusone <son.nguyen@plusonerobotics.com>
2022-06-26 15:28:35 -0500

    Fix env variable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,6 +5,7 @@ export const EMBED_MOVIE_SOURCE = process.env.EMBED_MOVIE_SOURCE || '';
 export const imageResize = (src: string, dimension: string = 'w200') =>
   `${TMDB_IMAGE}${dimension}${src}`;
 export const imageOriginal = (src: string) => `${TMDB_IMAGE}original${src}`;
-export const embedMovie = (id: number) => `${EMBED_MOVIE_SOURCE}/movie?id=${id}`;
+export const embedMovie = (id: number) =>
+  EMBED_MOVIE_SOURCE ? `${EMBED_MOVIE_SOURCE}/movie?id=${id}` : '';
 export const embedEpisode = (id: number, season: number, episode: number) =>
   `${EMBED_MOVIE_SOURCE}/tv?id=${id}&s=${season}&e=${episode}`;
