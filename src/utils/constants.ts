2301c1e3857bb56a2cda33e736ee1a25db07f676
sonplusone <son.nguyen@plusonerobotics.com>
2022-06-26 15:22:49 -0500

    Update env variable

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,9 +1,10 @@
 export const TMDB_API = 'https://api.themoviedb.org/3';
 export const TMDB_IMAGE = 'https://image.tmdb.org/t/p/';
+export const EMBED_MOVIE_SOURCE = process.env.EMBED_MOVIE_SOURCE || '';
 
 export const imageResize = (src: string, dimension: string = 'w200') =>
   `${TMDB_IMAGE}${dimension}${src}`;
 export const imageOriginal = (src: string) => `${TMDB_IMAGE}original${src}`;
-export const embedMovie = (id: number) => `https://www.2embed.ru/embed/tmdb/movie?id=${id}`;
+export const embedMovie = (id: number) => `${EMBED_MOVIE_SOURCE}/movie?id=${id}`;
 export const embedEpisode = (id: number, season: number, episode: number) =>
-  `https://www.2embed.ru/embed/tmdb/tv?id=${id}&s=${season}&e=${episode}`;
+  `${EMBED_MOVIE_SOURCE}/tv?id=${id}&s=${season}&e=${episode}`;
